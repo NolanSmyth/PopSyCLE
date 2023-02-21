8ced96a122049dc24a2c79a2dbc56c1fd92a3d87
Nolan Smyth <nolanwsmyth@gmail.com>
2023-02-21 22:02:00 +0000

    modularizing mock roman parameters

---
+++ docs/params.py
@@ -0,0 +1,19 @@
+#Make obs_time and area small for testing
+mock_roman_params = {"fdm": 1.0, 
+                     "pbh_mass":30, 
+                     "v_esc":550, 
+                     "rho_0":0.0093, 
+                     "r_s": 18.6, 
+                     "r_max":16.6 , 
+                     "gamma": 1.0, 
+                    #  "obs_time":1825, 
+                     "obs_time":18, 
+                     "cadence":3, 
+                     "blend_rad": 0.09, 
+                     "longitude": 1.00, 
+                     "latitude": -1.03, 
+                    #  "area": 0.16,
+                     "area": 0.0016,
+                     "BH_kick_speed_mean": 100,
+                     "NS_kick_speed_mean": 350
+                     }
